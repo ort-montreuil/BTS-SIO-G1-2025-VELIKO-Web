3e8ac49d517905d9f6056409dce1fc2df9ea3b86
Elie Chicha <eliechicha1@gmail.com>
2024-11-04 14:32:58 +0100

    Suppression Compte, Change bdd en anonyme dans nom,prenom,mail , Lien Decouvrir Veliko

---
+++ src/Controller/DecouvrirVelikoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DecouvrirVelikoController extends AbstractController
+{
+    #[Route('/decouvrir/veliko', name: 'app_decouvrir_veliko')]
+    public function index(): Response
+    {
+        return $this->render('decouvrir_veliko/index.html.twig', [
+            'controller_name' => 'DecouvrirVelikoController',
+        ]);
+    }
+}
--- src/Controller/EditController.php
+++ src/Controller/EditController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\User;
@@ -10,10 +9,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class EditController extends AbstractController
 {
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
     #[Route('/edit/{id}', name: 'app_edit', methods: ['GET', 'POST'])]
     public function edit(User $user, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
     {
@@ -39,6 +45,37 @@ class EditController extends AbstractController
         return $this->render('edit/index.html.twig', [
             'controller_name' => 'EditController',
             'form' => $form->createView(),
+            'user' => $user,
         ]);
+    }
+
+    #[Route('/delete/{id}', name: 'app_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
+    {
+        if (!$this->getUser() || $this->getUser()->getId() !== $user->getId()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Anonymiser l'utilisateur
+        $user->setNom('Anonyme');
+        $user->setPrenom('Anonyme');
+
+        // Générer une adresse e-mail anonyme unique
+        $emailAnonyme = 'anonyme_' . $user->getId() . '@example.com';
+        $user->setEmail($emailAnonyme);
+
+        // Hacher le mot de passe
+        $hashedPassword = bin2hex(random_bytes(10));
+        $user->setPassword($hashedPassword);
+
+        // Enregistrer les modifications dans la base de données
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Déconnexion de l'utilisateur
+        $this->tokenStorage->setToken(null);
+        $this->addFlash('success', 'Votre compte a été anonymisé avec succès. Vous avez été déconnecté.');
+
+        return $this->redirectToRoute('app_home');
     }
 }
