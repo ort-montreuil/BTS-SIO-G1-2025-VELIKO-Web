116614a8b605497d83d3332a4dcdb84b2eb331e2
UIshfaq <ishfaqumair93380@gmail.com>
2024-11-05 12:00:54 +0100

    fix bug bdd

--- migrations/Version20241105105526.php
+++ migrations/Version20241105105526.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241031082933 extends AbstractMigration
+final class Version20241105105526 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241031082933 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE date_naissance date_naissance DATE NOT NULL');
+        $this->addSql('ALTER TABLE user ADD confirmation_token VARCHAR(255) DEFAULT NULL, ADD is_verified SMALLINT DEFAULT 0 NOT NULL, CHANGE date_naissance date_naissance DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `user` CHANGE date_naissance date_naissance DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE `user` DROP confirmation_token, DROP is_verified, CHANGE date_naissance date_naissance DATE NOT NULL');
     }
 }
