c929b3da40d5afbec893c8bc25b486e802f9bc48
UIshfaq <ishfaqumair93380@gmail.com>
2024-11-06 17:43:37 +0100

    fix bug

--- migrations/Version20241106163649.php
+++ migrations/Version20241106163649.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241106143400 extends AbstractMigration
+final class Version20241106163649 extends AbstractMigration
 {
     public function getDescription(): string
     {
